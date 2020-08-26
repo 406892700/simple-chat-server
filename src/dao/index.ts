d844f0231f71b9974f273dee77a07527535c6752
徐怀远 <xuhy@shinemo.com>
2020-08-26 17:04:03 +0800

    feat: initial-commit

---
+++ src/dao/index.ts
@@ -0,0 +1,26 @@
+const fc = () => {
+  const name = 123
+
+  console.log(name)
+}
+
+interface b {
+  a: number,
+  b: string
+}
+
+const c:b = {
+  a: 2,
+  b: '3',
+}
+
+console.log(c)
+
+const a = {
+  a: 123,
+  b: 222,
+}
+
+console.log(a)
+
+fc()
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+const Koa = require('koa');
+
+const app = new Koa()
+const server = require('http').createServer(app.callback());
+const io = require('socket.io')(server, {
+  path: '/pollmsg',
+})
+
+const port = process.env.PORT || 3000
+
+io.on('connection', (socket:any) => {
+  socket.on('chat message', (msg:any) => {
+    io.emit('chat message', msg)
+  });
+});
+
+server.listen(port, () => {
+  console.log(`listening on *: ${port}`);
+});
