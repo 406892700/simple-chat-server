5caed2249895a382d2d72397681dd37f174b2750
徐怀远 <xuhy@shinemo.com>
2020-08-27 15:45:52 +0800

    feat: bugfix

--- dist/dao/index.js
+++ dist/dao/index.js
@@ -2,5 +2,15 @@ const fc = () => {
     const name = 123;
     console.log(name);
 };
+const c = {
+    a: 2,
+    b: '3',
+};
+console.log(c);
+const a = {
+    a: 123,
+    b: 222,
+};
+console.log(a);
 fc();
 //# sourceMappingURL=index.js.map
--- dist/server.js
+++ dist/server.js
@@ -1,34 +1,16 @@
-// import * as Koa from 'koa'
-// import * as Router from 'koa-router'
-// const http = require('http')
-// const app = new Koa()
-// const server = http.Server(app.callback())
-// const io = require('socket.io')(server)
-// const router = new Router()
-// router.get('/', async (ctx) => {
-//   ctx.body = 'Hello World!'
-// })
-// app.use(router.routes())
-// io.on('connection', (socket: any) => {
-//   // console.log('初始化成功！下面可以用socket绑定事件和触发事件了');
-//   socket.on('send', (data: any) => {
-//     console.log('客户端发送的内容：', data);
-//     socket.emit('getMsg', '我是返回的消息... ...');
-//   })
-// })
-// server.listen(3000, () => {
-//   console.log('Server running on port 3000')
-// })
-const server = require('http').createServer();
-const io = require('socket.io')(server);
-io.on('connection', (socket) => {
-    console.log(2222);
-    io.emit('chat message', 'ddddddd');
+const Koa = require('koa');
+const app = new Koa();
+const server = require('http').createServer(app.callback());
+const io = require('socket.io')(server, {
+    path: '/pollmsg',
 });
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     io.emit('chat message', msg);
-//   });
-// });
-server.listen(3002);
+const port = process.env.PORT || 3000;
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+server.listen(port, () => {
+    console.log(`listening on *: ${port}`);
+});
 //# sourceMappingURL=server.js.map
